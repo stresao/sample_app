482765c4ffa9b8981e36da398e90c56c70aaf041
Jon Arribas <jonarriba@gmail.com>
2012-08-05 13:58:49 +0200

    Make a basic User Model (including secure passwords)

--- Gemfile
+++ Gemfile
@@ -8,7 +8,9 @@ gem 'bootstrap-sass', '2.0.0'
 gem 'sqlite3', '1.3.5'
 gem 'rspec-rails', '2.10.0'
 #gem 'guard-rspec', '0.5.5'
+gem 'bcrypt-ruby', '3.0.1'
 
+gem 'annotate', '2.5.0', group: :development
 
 # Gems used only for assets and not required
 # in production environments by default.
--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -1,11 +1,11 @@
 require 'spec_helper'
 
-describe "Users" do
-  describe "GET /users" do
-    it "works! (now write some real specs)" do
-      # Run the generator again with the --webrat flag if you want to use webrat methods/matchers
-      get users_path
-      response.status.should be(200)
-    end
-  end
+describe User do
+
+  before { @user = User.new(name: "Example User", email: "user@example.com") }
+
+  subject { @user }
+
+  it { should respond_to(:name) }
+  it { should respond_to(:email) }
 end
